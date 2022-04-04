06ce361f8ab846fcfc15139e12f92822928ea1f6
kam <kamaltal123@gmail.com>
2022-04-04 17:11:49 +0000

    Add data to blog table, make data auto

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ $connect = mysqli_connect($host, $username, $password, $database);
 if (mysqli_connect_errno()) {
     echo "Connection failed to database";
 }
-//  else {
+// else {
 //     echo "Connection successful to databases";
 // }
 
@@ -40,12 +40,22 @@ if (mysqli_connect_errno()) {
                     $body_blog = $_POST['body_blog'];
                     $id_author_fk = $_POST['id_author_fk'];
                     $id_lecture_fk = $_POST['id_lecture_fk'];
+                    // $thumbnail_link = $_POST['thumbnail_link'];
+                    $query_insert = "INSERT INTO blog(title_blog, body_blog, id_author_fk, id_lecture_fk) VALUES('" .  $title_blog . "','" . $body_blog . "','" . $id_author_fk . "','" . $id_lecture_fk . "')";
+                    // $query = "INSERT INTO voiture(Matricule, Couleur, Typecar, Manuel, marque) VALUES ('" . $Matricule . "','" . $Couleur . "','" . $Typecar . "','" . $Manuel . "','" . $marque . "')";
+                    $result_insert = mysqli_query($connect, $query_insert);
+                    if (!$result_insert) {
+                        echo "<div>Data not insteted.</div><br>" . $query_insert;
+                    }
                 }
+
+
+
 
                 ?>
 
                 <h2>Add New Blog</h2>
-                <form action="" method="post" enctype="multypart/form-data">
+                <form action="#" method="post" enctype="multypart/form-data">
                     <div class="form-group mt-2">
                         <label for="title_blog">Blog Title</label>
                         <input class="form-control" type="text" name="title_blog">
@@ -55,13 +65,13 @@ if (mysqli_connect_errno()) {
 
                     <div class="form-group mt-2">
                         <label>Author : </label>
-                        <select name="id_author">
+                        <select name="id_author_fk">
                             <?php
                             $quesry_author = "select * from author";
                             $result_author = mysqli_query($connect, $quesry_author);
                             if (mysqli_num_rows($result_author) > 0) {
                                 while ($row = mysqli_fetch_assoc($result_author)) {
-                                    echo "<option name='" . $row['id_author'] . "'>" . $row['name_author'] . "</option>";
+                                    echo "<option value=" . $row['id_author'] . ">" . $row['name_author'] . "</option>";
                                 }
                             }
                             ?>
@@ -70,22 +80,25 @@ if (mysqli_connect_errno()) {
 
                     <div class="form-group mt-2">
                         <label>Lecture Category : </label>
-                        <select name="id_author">
+                        <select name="id_lecture_fk">
                             <?php
                             $quesry_lecture = "select * from lecture";
                             $result_lecture = mysqli_query($connect, $quesry_lecture);
                             if (mysqli_num_rows($result_lecture) > 0) {
                                 while ($row = mysqli_fetch_assoc($result_lecture)) {
-                                    echo "<option name='" . $row['id_lecture'] . "'>" . $row['name_lecture'] . "</option>";
+                                    echo "<option value=" . $row['id_lecture'] . ">" . $row['name_lecture'] . "</option>";
                                 }
                             }
                             ?>
                         </select>
                     </div>
-
+                    <!-- <div class="form-group mt-5">
+                        <label for="thumbnail_link">Thumbnail Image : </label>
+                        <input type="file" name="thumbnail_link">
+                    </div> -->
 
                     <div class="form-group mt-5">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
@@ -93,18 +106,13 @@ if (mysqli_connect_errno()) {
 
         </div>
     </section>
-
-
-
-
-
-
     <!-- script for ckeditor for uploading images -->
     <script>
-        CKEDITOR.replace('body_blog', {
-            height: 300,
-            filebrowserUploadUrl: "upload.php"
-        });
+        //     CKEDITOR.replace('body_blog', {
+        //         height: 300,
+        //         filebrowserUploadUrl: "upload.php"
+        //     });
+        // 
     </script>
 
 
