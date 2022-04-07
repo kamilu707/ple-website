57ced22fb9ecd84dbce3c2f739b9cc971f6ca1da
kam <kamaltal123@gmail.com>
2022-04-07 14:44:52 +0000

    Add functionality of trating answers & returning grade of quize

--- quize.php
+++ quize.php
@@ -35,28 +35,62 @@ include('base_top.php')
         <div class="col-md-8 bg-white ">
             <div class="row">
                 <h1>Quizes and Exercies</h1>
-                <?php
-                $query = "select * from quize where id_blog_fk=" . $_GET['idq'];
-                $result = mysqli_query($connect, $query);
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
+                <form action="" method="post">
+                    <?php
 
-                        echo "<h2>" . $row['title_quize'] . "</h2>";
-                        echo $row['content_quize'];
-                    }
-                    $query_exrecice = "SELECT * From exercice where id_blog_fk=" . $_GET['idq'];
-                    $result_exercice = mysqli_query($connect, $query_exrecice);
-                    if (mysqli_num_rows($result_exercice) > 0) {
-                        while ($row2 = mysqli_fetch_assoc($result_exercice)) {
-                            echo "<h3 class='bg-light p-2 mb-5'>" . $row2['question'] . "</h3>";
-                            echo "<input type='checkbox'>" . $row2['choice1'];
-                            echo "<input type='checkbox'>" . $row2['choice2'];
-                            echo "<input type='checkbox'>" . $row2['choice3'];
+                    $query = "select * from quize where id_blog_fk=" . $_GET['idq'];
+                    $result = mysqli_query($connect, $query);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+
+                            echo "<h2>" . $row['title_quize'] . "</h2>";
+                            echo $row['content_quize'];
+                        }
+                        $query_exrecice = "SELECT * From exercice where id_blog_fk=" . $_GET['idq'];
+                        $result_exercice = mysqli_query($connect, $query_exrecice);
+                        if (mysqli_num_rows($result_exercice) > 0) {
+                            $counter = 0;
+                            while ($row2 = mysqli_fetch_assoc($result_exercice)) {
+                                echo "<h3 class='bg-light p-2 mb-5'>" . $row2['question'] . "</h3>";
+                                echo "<div> <input type='radio' name='" . $counter . "'value='" . $row2['choice1'] .  "'>" . $row2['choice1'] . "</div>";
+                                echo "<div> <input type='radio' name='" .  $counter . "'value='" . $row2['choice2'] .  "'>" . $row2['choice2'] . "</div>";
+                                echo "<div> <input type='radio' name='" . $counter . "'value='" . $row2['choice3'] .  "'>" . $row2['choice3'] . "</div>";
+                                $counter++;
+                            }
                         }
                     }
-                }
-                ?>
+                    mysqli_free_result($result_exercice);
+                    ?>
+                    <input type="submit" name="submit" value="Check Answers">
+                </form>
             </div>
+
+            <!-- Now check the answers and send back the result Mark -->
+            <?php
+            if (isset($_POST['submit'])) {
+                $query_exrecice = "SELECT * From exercice where id_blog_fk=" . $_GET['idq'];
+                $result_exercice = mysqli_query($connect, $query_exrecice);
+                global $grade;
+                $grade = 1;
+                foreach ($_POST as $key => $value) {
+                    // echo $key . "<br>";
+                    echo $key . "<br>";
+                }
+                if (mysqli_num_rows($result_exercice) > 0) {
+                    $counter = 0;
+                    while ($row2 = mysqli_fetch_assoc($result_exercice)) {
+                        if ($_POST[$counter] == $row2['correct_resp']) {
+                            $grade += 1;
+                        }
+                        // echo $row2['question'] . "<br>";
+                        $counter++;
+                    }
+                }
+                // }
+            }
+            echo "You grade is: " . $grade;
+
+            ?>
         </div>
     </div>
 </section>
